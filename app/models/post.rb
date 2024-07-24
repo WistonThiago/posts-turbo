85704a1caef4df9a2249de08136920540db8fd16
Wiston Thiago <wthyago-2000@hotmail.com>
2024-07-24 18:26:37 -0300

    Updating the model to broadcast and change things on real time (prepend and channel).

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
   scope :ordered, -> { order(id: :desc) }
   validates :title, :body, presence: true
+
+  after_create_commit -> { broadcast_prepend_to "posts" }
+
 end
