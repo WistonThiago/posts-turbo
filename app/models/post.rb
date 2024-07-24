853dfc116495f78aa8486c766c4b6a66e528e8b5
Wiston Thiago <wthyago-2000@hotmail.com>
2024-07-24 18:58:37 -0300

    Using the helper  replacing the after_x_commit.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,10 @@ class Post < ApplicationRecord
   scope :ordered, -> { order(id: :desc) }
   validates :title, :body, presence: true
 
-  after_create_commit -> { broadcast_prepend_to "posts" }
-  after_update_commit -> { broadcast_replace_to "posts" }
-  after_destroy_commit -> { broadcast_remove_to "posts" }
+  #after_create_commit -> { broadcast_prepend_to "posts" }
+  #after_update_commit -> { broadcast_replace_to "posts" }
+  #after_destroy_commit -> { broadcast_remove_to "posts" }
+  
+  broadcasts_to -> (post) { "posts" }, inserts_by: :prepend
 
 end
